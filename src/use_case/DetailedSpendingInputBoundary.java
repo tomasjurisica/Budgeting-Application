084b5c6fd216fd3054ce4ae96af6b49dda34524b
avsarece <eceavsar@hotmail.com>
2025-11-26 17:18:02 -0500

    Implemented DetailedSpendingInputBoundary

--- src/use_case/DetailedSpendingInputBoundary.java
+++ src/use_case/DetailedSpendingInputBoundary.java
@@ -1,4 +1,10 @@
 package use_case;
 
 public interface DetailedSpendingInputBoundary {
+    /**
+     * Execute the Detailed Spending Use Case.
+     *
+     * @param detailedSpendingInputData the input data for this use case
+     */
+    void execute(DetailedSpendingInputData detailedSpendingInputData);
 }
