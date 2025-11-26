8d6c5b80bf1f0631b4da29d4b88a1e82150c5796
avsarece <eceavsar@hotmail.com>
2025-11-26 17:33:52 -0500

    Rest of interface adapter implemented

---
+++ src/interface_adapter/detailed_spending/DetailedSpendingPresenter.java
@@ -0,0 +1,29 @@
+package interface_adapter.detailed_spending;
+import use_case.detailed_spending.DetailedSpendingOutputBoundary;
+import use_case.detailed_spending.DetailedSpendingOutputData;
+
+public class DetailedSpendingPresenter implements DetailedSpendingOutputBoundary {
+    private final DetailedSpendingViewModel detailedSpendingViewModel;
+    public DetailedSpendingPresenter(DetailedSpendingViewModel detailedSpendingViewModel) {
+        this.detailedSpendingViewModel = detailedSpendingViewModel;
+    }
+    @Override
+    public void prepareSuccessView(DetailedSpendingOutputData response) {
+        final DetailedSpendingState detailedSpendingState = detailedSpendingViewModel.getState();
+        // detailedSpendingState.getPurchases(response.getPurchases());
+        detailedSpendingState.setCategoryName(response.getCategoryName());
+        detailedSpendingState.setPurchases(response.getPurchases());
+        detailedSpendingState.setHasError(false);
+        detailedSpendingState.setErrorMessage(null);
+        detailedSpendingViewModel.firePropertyChange();
+    }
+
+    @Override
+    public void prepareFailView(String errorMessage) {
+        final DetailedSpendingState detailedSpendingState = detailedSpendingViewModel.getState();
+        detailedSpendingState.setHasError(true);
+        detailedSpendingState.setErrorMessage(errorMessage);
+        detailedSpendingViewModel.firePropertyChange();
+    }
+
+}
