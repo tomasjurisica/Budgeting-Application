dc36fb725c1dccb37e44a52bba0006e257137fe8
clds13 <cindyy260027@gmail.com>
2025-11-28 18:56:42 -0500

    add JavaDoc comment on a class to abide to CheckStyle

--- src/main/java/use_case/add_user/AddUserInputBoundary.java
+++ src/main/java/use_case/add_user/AddUserInputBoundary.java
@@ -1,3 +1,8 @@
+/**
+ * The input boundary for the Add User use case.
+ * <p>
+ * This interface defines the request model for adding a new user.
+ */
 package use_case.add_user;
 
 public interface AddUserInputBoundary {
